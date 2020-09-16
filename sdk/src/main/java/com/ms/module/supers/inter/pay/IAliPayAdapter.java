b076600e7fe6f7e556f5e3542bb84621ad72da40
m_maohuawei <maohuawei@bdlbsc.com>
2020-09-16 17:39:04 +0800

    添加支付适配器

---
+++ sdk/src/main/java/com/ms/module/supers/inter/pay/IAliPayAdapter.java
@@ -0,0 +1,10 @@
+package com.ms.module.supers.inter.pay;
+
+import android.app.Activity;
+
+public class IAliPayAdapter implements IAliPay {
+    @Override
+    public void pay(Activity activity, PayCallBack callBack) {
+
+    }
+}
---
+++ sdk/src/main/java/com/ms/module/supers/inter/pay/IWeChatPayAdapter.java
@@ -0,0 +1,10 @@
+package com.ms.module.supers.inter.pay;
+
+import android.app.Activity;
+
+public class IWeChatPayAdapter implements IWeChatPay {
+    @Override
+    public void pay(Activity activity, PayCallBack callBack) {
+
+    }
+}
